192bf7fb73ac855362c309223a81480e2baf683e
heviztofurdovezeto <120281263+heviztofurdovezeto@users.noreply.github.com>
2023-05-31 11:28:13 +0000

    Add config.js to assets folder w/ it's content from rhetesi's repo

---
+++ assets/config.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const seasonTicket = {
+  20180401: {
+    600: { price: 8500, valid: 10 },
+    660: { price: 8500, valid: 10 },
+    1200: { price: 16000, valid: 20 },
+    1320: { price: 16000, valid: 20 },
+    deposit: 1500,
+  },
+  20190401: {
+    600: { price: 9000, valid: 10 },
+    660: { price: 9000, valid: 10 },
+    1200: { price: 17000, valid: 20 },
+    1320: { price: 17000, valid: 20 },
+    deposit: 1500,
+  },
+  20200401: {
+    600: { price: 10000, valid: 10 },
+    660: { price: 10000, valid: 10 },
+    1200: { price: 19000, valid: 20 },
+    1320: { price: 19000, valid: 20 },
+    deposit: 2000,
+  },
+  20220401: {
+    600: { price: 11000, valid: 15 },
+    660: { price: 11000, valid: 15 },
+    1200: { price: 20000, valid: 15 },
+    1320: { price: 20000, valid: 15 },
+    deposit: 2000,
+  },
+  20230401: {
+    600: { price: 13000, valid: 15 },
+    660: { price: 13000, valid: 15 },
+    1200: { price: 23000, valid: 15 },
+    1320: { price: 23000, valid: 15 },
+    deposit: 2000,
+  },
+};
+
+export { seasonTicket };
